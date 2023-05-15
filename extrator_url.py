35127e4d780c88f780da9ec10b4f5591847fc16e
Bruxeiro <gabrielrosabueno@hotmail.com>
2023-05-15 19:33:07 -0300

    Finalizando conversão, utilizando de elementos para validar URL

--- extrator_url.py
+++ extrator_url.py
@@ -3,38 +3,38 @@ import re
 
 class ExtratorURL:
     def __init__(self, url):
-        self.url = self.sanitizar_url(url)
-        self.validar_url()
+        self.url = self.sanitiza_url(url)
+        self.valida_url()
 
     @staticmethod
-    def sanitizar_url(url):
+    def sanitiza_url(url):
         if type(url) == str:
             return url.strip()
         else:
             return ""
 
-    def validar_url(self):
+    def valida_url(self):
         if not self.url:
             raise ValueError("A URL está vazia")
-        padrao_url = re.compile('(https://)?(www.)?bytebank.com?(.br)?/cambio')
-        match = padrao_url.match(url)
 
+        padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
+        match = padrao_url.match(url)
         if not match:
-            raise ValueError("A URL não é valida")
+            raise ValueError("A URL não é válida.")
 
     def get_url_base(self):
-        indice_interogacao = self.url.find("?")
-        url_base = self.url[:indice_interogacao]
+        indice_interrogacao = self.url.find('?')
+        url_base = self.url[:indice_interrogacao]
         return url_base
 
     def get_url_parametros(self):
-        indice_interogacao = self.url.find("?")
-        url_parametros = self.url[indice_interogacao + 1:]
+        indice_interrogacao = self.url.find('?')
+        url_parametros = self.url[indice_interrogacao + 1:]
         return url_parametros
 
-    def get_valor_parametro(self, parametro):
-        indice_parametro = self.get_url_parametros().find(parametro)
-        indice_valor = indice_parametro + len(parametro) + 1
+    def get_valor_parametro(self, parametro_busca):
+        indice_parametro = self.get_url_parametros().find(parametro_busca)
+        indice_valor = indice_parametro + len(parametro_busca) + 1
         indice_e_comercial = self.get_url_parametros().find('&', indice_valor)
         if indice_e_comercial == -1:
             valor = self.get_url_parametros()[indice_valor:]
@@ -42,9 +42,47 @@ class ExtratorURL:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
         return valor
 
+    def __len__(self):
+        return len(self.url)
 
-url = "bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
+    def __str__(self):
+        return self.url + "\n" + "Parâmetros: " + self.get_url_parametros() + "\n" + "URL Base: " + self.get_url_base()
 
-extrator_url = ExtratorURL(None)
+    def __eq__(self, other):
+        return self.url == other.url
+
+
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=dolar&moedaDestino=real"
+extrator_url = ExtratorURL(url)
+extrator_url_2 = ExtratorURL(url)
+
+'''print("O tamanho da URL é: ", len(extrator_url))
+print("URL completa: ", extrator_url)
+
+# Verifica que duas instâncias com a mesma URL são iguais
+print("extrator_url == extrator_url_2? ", extrator_url == extrator_url_2)
+
+# Busca o valor do parâmetro quantidade
 valor_quantidade = extrator_url.get_valor_parametro("quantidade")
-print(valor_quantidade)
+print("Valor do parâmetro 'quantidade': ", valor_quantidade)'''
+
+valor_dolar = 5.00
+moeda_origem = extrator_url.get_valor_parametro("moedaOrigem")
+moeda_destino = extrator_url.get_valor_parametro("moedaDestino")
+quantidade = extrator_url.get_valor_parametro("quantidade")
+
+
+if moeda_origem == "dolar" and moeda_destino == "real":
+    valor_conversao = (float(quantidade)*valor_dolar)
+    print("O valor de U$ " + quantidade + " dolares, é de R$ " + str(valor_conversao) + " reais")
+
+elif moeda_origem == "real" and moeda_destino == "dolar":
+    valor_conversao = (float(quantidade)/valor_dolar)
+    print("O valor de R$ " + quantidade + " reais, é de U$ " + str(valor_conversao) + " dolares")
+
+
+else:
+    print("Operação de conversão não disponível, digite um valor em dólares para converter em reais")
+
+
+
--- main.py
+++ main.py
@@ -1,24 +1,20 @@
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 
+# Sanitização da URL
+url = url.strip()
 
-url = "bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
-
-#Sanitização da URL
-url + url.strip()
-
-
-#Validação da URL
+# Validação da URL
 if url == "":
-    raise ValueError("URL Vazia!!!")
+    raise ValueError("A URL está vazia")
 
-
-#Separar base de parametros
+# Separa base e parâmetros
 indice_interrogacao = url.find('?')
 url_base = url[:indice_interrogacao]
 url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
 
-#Busca o valor de um parametro
-parametro_busca = 'moedaOrigem'
+# Busca o valor de um parâmetro
+parametro_busca = 'quantidade'
 indice_parametro = url_parametros.find(parametro_busca)
 indice_valor = indice_parametro + len(parametro_busca) + 1
 indice_e_comercial = url_parametros.find('&', indice_valor)
@@ -27,4 +23,4 @@ if indice_e_comercial == -1:
 else:
     valor = url_parametros[indice_valor:indice_e_comercial]
 
-print (valor)
+print(valor)
